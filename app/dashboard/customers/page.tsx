4ecfb4838158a3ccae2f6273450fad2d1ae41135
naijoesrinivasan <naijoesrinivasan99@gmail.com>
2024-09-21 21:23:53 -0700

    added customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,25 @@
-export default function Page()  {
-  return <h1></h1>
+import { fetchAllCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import { FormattedCustomersTable } from "@/app/lib/definitions";
+import { lusitana } from "@/app/ui/fonts";
+import { custom } from "zod";
+
+// export type FormattedCustomersTable = {
+//   id: string;
+//   name: string;
+//   email: string;
+//   image_url: string;
+//   total_invoices: number;
+//   total_pending: string;
+//   total_paid: string;
+// };
+
+export default async function Page()  {
+  const customers = await fetchAllCustomers()
+
+  return (
+    <section className="">
+      <CustomersTable customers={customers}/>
+    </section>
+  )
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,7 +1,9 @@
 import { sql } from '@vercel/postgres';
 import {
+  Customer,
   CustomerField,
   CustomersTableType,
+  FormattedCustomersTable,
   InvoiceForm,
   InvoicesTable,
   LatestInvoiceRaw,
@@ -218,5 +220,28 @@ export async function fetchFilteredCustomers(query: string) {
   } catch (err) {
     console.error('Database Error:', err);
     throw new Error('Failed to fetch customer table.');
+  }
+}
+
+export async function fetchAllCustomers()  {
+  try {
+    const data = await sql<FormattedCustomersTable>`
+      SELECT
+		  customers.id,
+		  customers.name,
+		  customers.email,
+		  customers.image_url,
+		  COUNT(invoices.id) AS total_invoices,
+		  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
+		  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
+		  FROM customers
+		  LEFT JOIN invoices ON customers.id = invoices.customer_id
+		  GROUP BY customers.id, customers.name, customers.email, customers.image_url
+		        ORDER BY customers.name ASC`
+    console.log(data.rows)
+    return data.rows;
+  } catch (error) {
+    console.log('Database Error: ', error);
+    throw new Error('Failed to fetch customer data');
   }
 }
