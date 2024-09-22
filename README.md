c73e69ed63fb6e1c76adfb90437e05b9dffaf936
naijoesrinivasan <naijoesrinivasan99@gmail.com>
2024-09-21 21:28:38 -0700

    cleaned up more Eslint errors

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 This is a Next.js project created from the starter template for the Next.js App Router Course. 
 
+1. Click on the hint button to reveal the username and password
+2. Try not to modify the data although it can be modified 
+
 
--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,18 +1,6 @@
 import { fetchAllCustomers } from "@/app/lib/data";
 import CustomersTable from "@/app/ui/customers/table";
-import { FormattedCustomersTable } from "@/app/lib/definitions";
-import { lusitana } from "@/app/ui/fonts";
-import { custom } from "zod";
 
-// export type FormattedCustomersTable = {
-//   id: string;
-//   name: string;
-//   email: string;
-//   image_url: string;
-//   total_invoices: number;
-//   total_pending: string;
-//   total_paid: string;
-// };
 
 export default async function Page()  {
   const customers = await fetchAllCustomers()
